ea9311e2c23d85223ce68195fa37d8bce54d82b4
Huzaifa Iqbal <huzaifaiqb005@gmail.com>
2026-07-30 16:50:56 +0500

    Time: 0 ms (100%), Space: 35.8 MB (56.4%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,41 +1,17 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        std::vector<int> left(nums.size());
-        std::vector<int> right(nums.size());
-        if(nums.size() == 1) {
-            return 0;
-        }
+        int left = 0;
+        int right = 0;
         for(int i = 0; i < nums.size(); i++) {
-            if(i == 0) {
-                left[i] = nums[i];
-            }
-            else {
-                left[i] = left[i - 1] + nums[i];
-            }
+            right += nums[i];
         }
-        for(int i = nums.size() - 1; i >= 0; i--) {
-            if(i == nums.size() - 1) {
-                right[i] = nums[i];
+        for(int j = 0; j < nums.size(); j++) {
+            right -= nums[j];
+            if(right == left) {
+                return j;
             }
-            else {
-                right[i] = right[i + 1] + nums[i];
-            }
-        }
-        for(int k = 0; k < nums.size(); k++) {
-            if(k == 0) {
-                if(right[k + 1] == 0) {
-                    return k;
-                }
-            }            
-            else if(k == nums.size() - 1) {
-                if(left[k - 1] == 0) {
-                    return k;
-                }
-            }    
-            else if(left[k - 1] == right[k + 1]) {
-                return k;
-            }
+            left += nums[j];
         }
         return -1;
     }
