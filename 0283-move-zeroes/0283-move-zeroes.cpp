b2e09713a6b9415a752b3e6d4faeca46025775c0
Huzaifa Iqbal <huzaifaiqb005@gmail.com>
2026-08-03 20:14:35 +0500

    Time: 0 ms (100%), Space: 24 MB (19.76%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+        for(int i = 1; i < nums.size(); i++) {
+            if(nums[j] == 0 && nums[i] != 0) {
+                std::swap(nums[j], nums[i]);
+            }
+            else if(nums[j] == 0 && nums[i] == 0) {
+                continue;
+            }
+            j++;
+        }
+    }
+};
