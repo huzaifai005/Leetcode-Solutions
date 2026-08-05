ff765fbdb436c5113fd793c94cddd608ea3721ae
Huzaifa Iqbal <huzaifaiqb005@gmail.com>
2026-08-05 17:35:31 +0500

    Time: 0 ms (100%), Space: 8.7 MB (33.23%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
+        int j = 0;
         if(s.size() == 0) {
             return true;
         }
-        int j = 0;
         for(int i = 0; i < t.size(); i++) {
             if(j == s.size() - 1 && s[j] == t[i]) {
                 return true;
