711cc94ebac245fd8fb5d6d6b8ccc3936ed12723
Huzaifa Iqbal <huzaifaiqb005@gmail.com>
2026-08-05 19:29:51 +0500

    Time: 3 ms (34.41%), Space: 63 MB (49.69%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -4,7 +4,7 @@ public:
         int j = 0;
         int i = height.size() - 1;
         int area = 0;
-        while(i > j) {
+        while(j < i) {
             area = std::max(std::abs((i + 1) - (j + 1))*std::min(height[i], height[j]), area);
             if(height[i] > height[j]) {
                 j++;
