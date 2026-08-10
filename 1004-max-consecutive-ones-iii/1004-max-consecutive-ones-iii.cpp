6fcdc7cf378f3be5f7e27d24c1a010ab96ab8973
Huzaifa Iqbal <huzaifaiqb005@gmail.com>
2026-08-10 16:22:46 +0500

    Time: 0 ms (100%), Space: 69.7 MB (34.74%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -6,16 +6,18 @@ public:
         int count = 0;
         for(int i = 0; i < nums.size(); i++) {
             if(nums[i] == 0) {
+                if(count != k) {
                     count++;
                 }
-                while(count > k) {
-                    if(nums[j] == 0) {
-                        count--;
+                else {
+                    while(nums[j] == 1) {
+                        j++;
                     }
                     j++;
                 }
-            window = std::max(window, i - j + 1);
             }
+            window = std::max(window, i - j + 1);
+        }
         return window;
     }
 };
