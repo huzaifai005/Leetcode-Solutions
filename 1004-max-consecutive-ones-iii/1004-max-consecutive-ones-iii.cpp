e42e94a6673339089e676e40cb683e87dbc52145
Huzaifa Iqbal <huzaifaiqb005@gmail.com>
2026-08-10 16:14:19 +0500

    Time: 3 ms (32.91%), Space: 69.7 MB (34.74%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -3,11 +3,10 @@ public:
     int longestOnes(vector<int>& nums, int k) {
         int j = 0;
         int window = 0;
-        int count = 0;
         for(int i = 0; i < nums.size(); i++) {
             if(nums[i] == 0) {
-                if(count != k) {
-                    count++;
+                if(k != 0) {
+                    k--;
                 }
                 else {
                     while(nums[j] == 1) {
