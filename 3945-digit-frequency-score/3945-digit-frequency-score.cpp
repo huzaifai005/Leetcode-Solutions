cea6cb3aeed8a043f13551a2d61bff6b4d22c9f2
Huzaifa Iqbal <huzaifaiqb005@gmail.com>
2026-08-11 17:54:36 +0500

    Time: 0 ms (100%), Space: 8.5 MB (93.93%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.cpp
+++ 3945-digit-frequency-score/3945-digit-frequency-score.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     int digitFrequencyScore(int n) {
-        std::unordered_map<int,int> map1;
         int total = 0;
         while(n > 0) {
-            map1[n%10]++;
+            total += n%10;
             n = n/10;
-        }
-        for(auto& pair : map1) {
-            total += pair.first*pair.second;
         }
         return total;
     }
