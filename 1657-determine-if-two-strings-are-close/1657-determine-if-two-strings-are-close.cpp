fd243ab6b3567844417daa77c7149dbbd3fc8ca1
Huzaifa Iqbal <huzaifaiqb005@gmail.com>
2026-08-14 16:18:50 +0500

    Time: 58 ms (5.78%), Space: 24.2 MB (5.69%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
-    bool closeStrings(std::string word1, std::string word2) {
-        std::vector<int> freq1(26, 0);
-        std::vector<int> freq2(26, 0);
-
-        for (char ch : word1) {
-            freq1[ch - 'a']++;
+    bool closeStrings(string word1, string word2) {
+        if(word1.size() != word2.size()) {
+            return 0;
         }
-
-        for (char ch : word2) {
-            freq2[ch - 'a']++;
+        std::set<char> set1(word1.begin(), word1.end());
+        std::set<char> set2(word2.begin(), word2.end());
+        if(set1 != set2) {
+            return 0;
         }
-
-        for (int i = 0; i < 26; i++) {
-            if ((freq1[i] == 0 && freq2[i] != 0) || (freq1[i] != 0 && freq2[i] == 0)) {
-                return false;
-            }
+        std::unordered_map<char, int> map1;
+        std::unordered_map<char, int> map2;
+        for(int i = 0; i < word1.size(); i++) {
+            map1[word1[i]]++;
+            map2[word2[i]]++;
         }
-
-        std::sort(freq1.begin(), freq1.end());
-        std::sort(freq2.begin(), freq2.end());
-
-        for (int i = 0; i < 26; i++) {
-            if (freq1[i] != freq2[i]) {
-                return false;
-            }
+        std::vector<int> v1;
+        std::vector<int> v2;
+        for(auto& pair : map1) {
+            v1.push_back(pair.second);
         }
-
-        return true;
+        for(auto& pair : map2) {
+            v2.push_back(pair.second);
+        }
+        std::sort(v1.begin(), v1.end());
+        std::sort(v2.begin(), v2.end());
+        if(v1 != v2) {
+            return 0;
+        }
+        return 1;
     }
 };
-
-
