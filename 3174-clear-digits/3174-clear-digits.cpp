eb0e3839cab7bf6308be10cc317e8ab6bdf6577b
Huzaifa Iqbal <huzaifaiqb005@gmail.com>
2026-08-15 21:11:13 +0500

    Time: 0 ms (100%), Space: 8.4 MB (68.45%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.cpp
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -1,30 +1,17 @@
 class Solution {
 public:
     string clearDigits(string s) {
-        std::stack<char> stack1;
+        std::string s2;
         for(int i = 0; i < s.size(); i++) {
-            if(s[i] >= '0' && s[i] <= '9') {
-                if(stack1.empty()) {
-                    continue;
-                }
-                else {
-                    stack1.pop();
+            if(isdigit(s[i])) {
+                if(!s2.empty()) {
+                    s2.pop_back();
                 }
             }
             else {
-                stack1.push(s[i]);
+                s2.push_back(s[i]);
             }
         }
-        if(stack1.empty()) {
-            return "";
-        }
-        int size = stack1.size();
-        for(int i = 0; i < size; i++) {
-            s[i] = stack1.top();
-            stack1.pop();
-        }
-        s.resize(size);
-        std::reverse(s.begin(), s.end());
-        return s;
+        return s2;
     }
 };
