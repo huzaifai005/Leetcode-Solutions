4cab69c765e18dab57d6ab91f9cea7708e804794
Huzaifa Iqbal <huzaifaiqb005@gmail.com>
2026-08-17 18:56:06 +0500

    Time: 0 ms (100%), Space: 8.3 MB (86.62%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int depth = 0;
+        int maxdepth = 0;
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == '(') {
+                depth++;
+            }
+            else if(s[i] == ')') {
+                depth--;
+            }
+            maxdepth = std::max(maxdepth, depth);
+        }
+        return maxdepth;
+    }
+};
