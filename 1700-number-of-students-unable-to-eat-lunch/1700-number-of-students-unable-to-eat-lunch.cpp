e45d9946b2a839d5445ab819641cf25455163661
Huzaifa Iqbal <huzaifaiqb005@gmail.com>
2026-08-18 16:52:49 +0500

    Time: 0 ms (100%), Space: 11.7 MB (70.61%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -1,31 +1,23 @@
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
-        std::queue<int> queue;
-        int temp = 0;
-        int count = students.size();
-        int rej = 0;
-        std::reverse(sandwiches.begin(), sandwiches.end());
-        for(int num : students) {
-            queue.push(num);
+        vector<int> counts(2, 0);
+        for (int student : students)
+            counts[student]++;
+
+        int remaining = sandwiches.size();
+        for (int sandwich : sandwiches) {
+            if (counts[sandwich] == 0)
+                break;
+            if (remaining == 0)
+                break;
+            counts[sandwich]--;
+            remaining--;
         }
-        while(!sandwiches.empty()) {
-            if(queue.front() == sandwiches.back()) {
-                queue.pop();
-                sandwiches.pop_back();
-                rej = 0;
-                count--;
-            }
-            else {
-                temp = queue.front();
-                queue.pop();
-                queue.push(temp);
-                rej++;
-                if(rej == count) {
-                    break;
-                }
-            }
-        }
-        return queue.size();
+
+        return remaining;
     }
 };
