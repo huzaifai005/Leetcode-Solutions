de4198b170d17b7b0b43029f1ee1d51ba8167289
Huzaifa Iqbal <huzaifaiqb005@gmail.com>
2026-09-05 16:51:02 +0500

    Time: 0 ms (100%), Space: 9.9 MB (90.89%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        // Both runners start at the very beginning of the chain
         ListNode* slow = head;
         ListNode* fast = head;
-        
-        // Fast runner needs to take 2 steps, so we must make sure
-        // that neither the current node nor the next node is null!
         while (fast != nullptr && fast->next != nullptr) {
-            slow = slow->next;         // Slow takes 1 step
-            fast = fast->next->next;   // Fast takes 2 steps
+            slow = slow->next;        
+            fast = fast->next->next;   
         }
-        
-        // When fast hits the end, slow is standing exactly in the middle
         return slow; 
     }
 };
